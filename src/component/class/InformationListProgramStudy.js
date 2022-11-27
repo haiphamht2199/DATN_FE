a81467a2ddd2e6c305c7a46490518496f6141246
Pham hai <haipd@bsscommerce.com>
2022-11-27 21:16:21 +0700

    add code

--- src/component/class/InformationListProgramStudy.js
+++ src/component/class/InformationListProgramStudy.js
@@ -1,4 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { Paper, Button } from '@mui/material';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
@@ -7,43 +13,55 @@ import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import AutorenewIcon from '@mui/icons-material/Autorenew';
 import { DataGrid } from '@mui/x-data-grid';
 import SettingsIcon from '@mui/icons-material/Settings';
+import ListProgramStudy from '../tables/ListProgramStudy';
+function createData(name, calories, fat, carbs, protein) {
+ return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+ createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+ createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+ createData('Eclair', 262, 16.0, 24, 6.0),
+ createData('Cupcake', 305, 3.7, 67, 4.3),
+ createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 function InformationListProgramStudy() {
- const columns = [
-  { field: 'id', headerName: 'STT', width: 70 },
-  { field: 'nameProgram', headerName: 'Tên chương trình', width: 200 },
-  { field: 'createUser', headerName: 'Người tạo', width: 200 },
-  {
-   field: 'startDate',
-   headerName: 'Thời gian tạo',
+ // const columns = [
+ //  { field: 'id', headerName: 'STT', width: 70 },
+ //  { field: 'nameProgram', headerName: 'Tên chương trình', width: 200 },
+ //  { field: 'createUser', headerName: 'Người tạo', width: 200 },
+ //  {
+ //   field: 'startDate',
+ //   headerName: 'Thời gian tạo',
 
-   width: 200,
-  },
-  {
-   field: 'editDate',
-   headerName: 'Thời gian chỉnh sửa',
-   description: 'This column has a value getter and is not sortable.',
-   sortable: false,
-   width: 200,
-  },
-  {
-   field: 'status',
-   headerName: 'Trạng thái',
+ //   width: 200,
+ //  },
+ //  {
+ //   field: 'editDate',
+ //   headerName: 'Thời gian chỉnh sửa',
+ //   description: 'This column has a value getter and is not sortable.',
+ //   sortable: false,
+ //   width: 200,
+ //  },
+ //  {
+ //   field: 'status',
+ //   headerName: 'Trạng thái',
 
-   width: 150,
-  },
-  {
-   field: 'action',
-   headerName: 'Thao thác',
-   width: 150,
-   SourceBuffer: true
-  },
- ];
+ //   width: 150,
+ //  },
+ //  {
+ //   field: 'action',
+ //   headerName: 'Thao thác',
+ //   width: 150,
+ //   SourceBuffer: true
+ //  },
+ // ];
 
- const rows = [
-  { id: 1, nameProgram: 'Chương trình số 1 ', createUser: 'Phạm Đình Hải', startDate: "10:00,29/10/2022", editDate: "10:00,30/10/2022", status: "Hoạt động", action: <SettingsIcon /> },
-  { id: 2, nameProgram: 'Chương trình số 2 ', createUser: 'Vương Xuân Hiệu', startDate: "10:00,29/10/2022", editDate: "10:00,30/10/2022", status: "Hoạt động", action: "cài đặt" },
-  { id: 3, nameProgram: 'Chương trình số 3 ', createUser: 'Phạm Đình Hải', startDate: "10:00,29/10/2022", editDate: "10:00,30/10/2022", status: "Hoạt động", action: "cài đặt" },
- ];
+ // const rows = [
+ //  { id: 1, nameProgram: 'Chương trình số 1 ', createUser: 'Phạm Đình Hải', startDate: "10:00,29/10/2022", editDate: "10:00,30/10/2022", status: "Hoạt động", action: <SettingsIcon /> },
+ //  { id: 2, nameProgram: 'Chương trình số 2 ', createUser: 'Vương Xuân Hiệu', startDate: "10:00,29/10/2022", editDate: "10:00,30/10/2022", status: "Hoạt động", action: "cài đặt" },
+ //  { id: 3, nameProgram: 'Chương trình số 3 ', createUser: 'Phạm Đình Hải', startDate: "10:00,29/10/2022", editDate: "10:00,30/10/2022", status: "Hoạt động", action: "cài đặt" },
+ // ];
 
  return (
   <div className='InformationListProgramStudy'>
@@ -88,15 +106,7 @@ function InformationListProgramStudy() {
     </div>
    </div>
    <div className='ListProgramStudyContent'>
-    <div style={{ height: 400, width: '100%' }}>
-     <DataGrid
-      rows={rows}
-      columns={columns}
-      pageSize={5}
-      rowsPerPageOptions={[5]}
-
-     />
-    </div>
+    <ListProgramStudy />
    </div>
   </div>
  )
